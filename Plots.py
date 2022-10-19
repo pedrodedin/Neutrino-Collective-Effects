ec3a0197293129b52c0680b36e7000501482e5f0
pedrodedin <pedroneto293@gmail.com>
2022-10-19 11:36:22 -0300

    Fast Oscillations Update

--- Plots.py
+++ Plots.py
@@ -9,8 +9,8 @@ from Auxiliar_Functions import *
 import os
 os.environ['PATH'] = os.environ['PATH'] + ':/Library/TeX/texbin/'
 import matplotlib.colors as mcolors
-plt.style.use(['science',"std-colors"])
-# plt.rcParams['axes.prop_cycle'] = plt.cycler(color=mcolors.TABLEAU_COLORS)
+plt.style.use(['science',"high-vis"])
+plt.rcParams['axes.prop_cycle'] = plt.cycler(color=mcolors.TABLEAU_COLORS)
 # plt.style.use('default')
 # plt.rcParams['text.usetex'] = True
 plt.rcParams['font.size'] = 15
